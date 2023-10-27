54b41a20cb863e74df200f8812218b795cea5dd6
ajr <reanojohn@gmail.com>
2023-10-27 19:14:16 +1000

    Refactor code to use promises exclusively

--- Projects/weather-app/main.js
+++ Projects/weather-app/main.js
@@ -1,47 +1,82 @@
 const weatherName = document.querySelector('.weather__location');
 const weatherIcon = document.querySelector('.weather__icon');
+const weatherCondition = document.querySelector('.weather__condition');
+const weatherSum = document.querySelector('.weather__summary');
 const weatherTemp = document.querySelector('.temp');
-const weatherTempFeel = document.querySelector('.temp__feel');
+const weatherTempFeel = document.querySelector('.weather__feel');
 const searchInput = document.querySelector('#location-input');
 const submitBtn = document.querySelector('.btn');
 
 const apiKey = '0aee69b209f94d2fb0930509231510';
 const apiURL = 'http://api.weatherapi.com/v1/current.json?key=';
 
-// Function to return weather JSON data from API.
-async function weatherData(location = 'Australia') {
-  let response = await fetch(apiURL + apiKey + `&q=${location}&aqi=no`, {
+const updateDOMText = (location) => {
+  weatherTemp.innerText = `${location.temp}°`;
+  weatherName.innerText = `${location.name}`;
+  weatherCondition.innerText = location.condition;
+  weatherTempFeel.innerText = `Feels like ${location.tempFeel}°`;
+};
+
+const updateWeather = (location = 'Australia') => {
+  fetch(apiURL + apiKey + `&q=${location}&aqi=no`, {
     mode: 'cors',
-  });
-  return await response.json();
-}
+  })
+  // Return JSON data
+    .then((response) => {
+      return response.json();
+    })
+  // Process JSON data
+    .then((weather) => {
+      return {
+        name: weather.location.name,
+        country: weather.location.country,
+        condition: weather.current.condition.text,
+        icon: weather.current.condition.icon,
+        temp: weather.current.temp_c,
+        tempFeel: weather.current.feelslike_c,
+      };
+    })
+    // Update weather
+    .then((locationWeather) => {
+      updateDOMText(locationWeather);
+    });
+};
+
+/* ASYNC VERSION
+
+// Function to return weather JSON data from API.
+// const weatherData = async (location = 'Australia') => {
+//   let response = await fetch(apiURL + apiKey + `&q=${location}&aqi=no`, {
+//     mode: 'cors',
+//   });
+//   return await response.json();
+// };
 
 // Process JSON data to simplified object for DOM manipulation.
-async function processData(location) {
-  let weather = await weatherData(location);
-  return {
-    name: weather.location.name,
-    country: weather.location.country,
-    condition: weather.current.condition.text,
-    icon: weather.current.condition.icon,
-    temp: weather.current.temp_c,
-    tempFeel: weather.current.feelslike_c,
-  };
-}
+// const process = (weather) => {
+//     return {
+//       name: weather.location.name,
+//       country: weather.location.country,
+//       condition: weather.current.condition.text,
+//       icon: weather.current.condition.icon,
+//       temp: weather.current.temp_c,
+//       tempFeel: weather.current.feelslike_c,
+//     };
+//   };
 
-//Update weather based on passed argument.
-async function updateWeather(location) {
-  let weather = await processData(location);
-  weatherName.innerText = `${weather.name},  ${weather.country}`;
-  weatherIcon.src = weather.icon;
-  weatherTemp.innerText = `${weather.temp}°`;
-  weatherTempFeel.innerText = `Feels like ${weather.tempFeel}°`;
-}
+//   //Update weather based on passed argument.
+//   const updateWeather = async (searchedLocation) => {
+//     let rawJSON = await weatherData(searchedLocation);
+//     let locationWeather = process(rawJSON);
+//     updateDOMText(locationWeather);
+//   };
+
+*/
 
 //Init
 updateWeather();
 
-submitBtn.addEventListener('click', async () => {
-  await updateWeather(searchInput.value);
+submitBtn.addEventListener('click', () => {
+  updateWeather(searchInput.value);
   searchInput.value = '';
 });
